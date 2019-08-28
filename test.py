32a8da469b8527c5498d50ba7a1af54c5d375d19
luyanger1799 <luyanger1799@qq.com>
2019-08-28 10:10:49 +0800

    fix test.py

--- test.py
+++ test.py
@@ -88,4 +88,6 @@ print("")
 steps = len(test_image_names) // args.batch_size
 
 # testing
-net.evaluate_generator(test_generator, steps=steps, workers=os.cpu_count(), use_multiprocessing=True)
+scores = net.evaluate_generator(test_generator, steps=steps, workers=os.cpu_count(), use_multiprocessing=True)
+
+print('loss={loss:0.4f}, MeanIoU={mean_iou:0.4f}'.format(loss=scores[0], mean_iou=scores[1]))
