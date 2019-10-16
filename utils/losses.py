b8f2ace70617449d838c2ba6a441a5a426b38a6d
luyanger1799 <luyanger1799@qq.com>
2019-10-16 09:35:21 +0800

    add self_balanced_focal_loss

--- utils/losses.py
+++ utils/losses.py
@@ -37,7 +37,7 @@ def miou_loss(weights=None, num_classes=2):
         assert len(weights) == num_classes
         weights = tf.convert_to_tensor(weights)
     else:
-        weights = tf.convert_to_tensor([1.]*num_classes)
+        weights = tf.convert_to_tensor([1.] * num_classes)
 
     def loss(y_true, y_pred):
         y_pred = backend.softmax(y_pred)
@@ -49,5 +49,39 @@ def miou_loss(weights=None, num_classes=2):
         union = backend.sum(union, axis=[1, 2])
 
         return -backend.mean((weights * inter) / (weights * union + 1e-8))
+
+    return loss
+
+
+def self_balanced_focal_loss(alpha=3, gamma=2.0):
+    """
+    Original by Yang Lu:
+
+    This is an improvement of Focal Loss, which has solved the problem
+    that the factor in Focal Loss failed in semantic segmentation.
+    It can adaptively adjust the weights of different classes in semantic segmentation
+    without introducing extra supervised information.
+
+    :param alpha: The factor to balance different classes in semantic segmentation.
+    :param gamma: The factor to balance different samples in semantic segmentation.
+    :return:
+    """
+
+    def loss(y_true, y_pred):
+        # cross entropy loss
+        y_pred = backend.softmax(y_pred, -1)
+        cross_entropy = backend.categorical_crossentropy(y_true, y_pred)
+
+        # sample weights
+        sample_weights = backend.max(backend.pow(1.0 - y_pred, gamma) * y_true, axis=-1)
+
+        # class weights
+        pixel_rate = backend.sum(y_true, axis=[1, 2], keepdims=True) / backend.sum(backend.ones_like(y_true),
+                                                                                   axis=[1, 2], keepdims=True)
+        class_weights = backend.max(backend.pow(backend.ones_like(y_true) * alpha, pixel_rate) * y_true, axis=-1)
+
+        # final loss
+        final_loss = class_weights * sample_weights * cross_entropy
+        return backend.mean(backend.sum(final_loss, axis=[1, 2]))
 
     return loss
